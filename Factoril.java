413f9677b7eae1acebcac0796172ee724fa679b1
unknown <shrishail.xworkz@gmail.cim>
2023-08-22 11:04:26 +0530

    commiting add data type variable program

---
+++ Factoril.java
@@ -0,0 +1,20 @@
+class Factoril{
+
+public static void main(String[]args){
+int factNumber=5;
+
+int dummy=1;
+for (int fact=1 ;fact<=factNumber;fact++)
+{
+dummy =dummy *fact;
+
+}
+System.out.println(" factNumber"+dummy);
+
+
+
+
+}
+
+
+}
