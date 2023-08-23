c268a43f31565aecbe13f5c1f6d053e33782c383
unknown <shrishail.xworkz@gmail.cim>
2023-08-23 17:35:06 +0530

    adding overloading program

---
+++ Speaker1Exicouter.java
@@ -0,0 +1,22 @@
+class Speaker1Exicouter{
+
+
+public static void main(String jbl[]){
+
+Speaker1.onoroff();
+Speaker1.increasevolume();
+Speaker1.decreasevalume();
+
+Speaker1.decreasevalume();
+Speaker1.decreasevalume();
+Speaker1.decreasevalume();
+
+Speaker1.decreasevalume();
+Speaker1.decreasevalume();
+Speaker1.decreasevalume();
+
+Speaker1.onoroff();
+}
+
+
+}
